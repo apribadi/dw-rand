f6102127c4711216324c925e8abac5b4d159a865
Aaron Pribadi <aaron.pribadi@gmail.com>
2023-01-16 13:10:33 -0800

    z

--- examples/rng.rs
+++ examples/rng.rs
@@ -1,14 +1,13 @@
-use core::num::NonZeroU128;
 use std::io::Write;
 use xox_random::Rng;
 
 fn main() {
-  let mut rng = Rng::new([117u8; 16]);
-  let mut buf = vec![0u8; 8192].into_boxed_slice();
+  let mut g = Rng::new(*b"autovivification");
   let mut out = std::io::stdout().lock();
+  let a = &mut [0u8; 4096];
 
   loop {
-    rng.fill(&mut buf);
-    out.write_all(&buf).expect("write_all failed!");
+    g.fill(a);
+    if let Err(_) = out.write_all(a) { break; }
   }
 }
--- tests/unified/main.rs
+++ tests/unified/main.rs
@@ -3,9 +3,9 @@ use xox_random::Rng;
 
 #[test]
 fn test() {
-  let mut g = Rng::new([117u8; 16]);
-  expect!["0xaa14111213e90a24"].assert_eq(&format!("{:#x}", g.next()));
-  expect!["0xa76761fd02073e41"].assert_eq(&format!("{:#x}", g.next()));
-  expect!["0x55bd88c08f434382"].assert_eq(&format!("{:#x}", g.next()));
-  expect!["0x56ec0708a27b1d26"].assert_eq(&format!("{:#x}", g.next()));
+  let mut g = Rng::new(*b"autovivification");
+  expect!["0xe6cb90eb01058266"].assert_eq(&format!("{:#x}", g.next()));
+  expect!["0x2d8ce79534418c6f"].assert_eq(&format!("{:#x}", g.next()));
+  expect!["0x619cdce6110c5a54"].assert_eq(&format!("{:#x}", g.next()));
+  expect!["0xaca4ba9f7d51d010"].assert_eq(&format!("{:#x}", g.next()));
 }
